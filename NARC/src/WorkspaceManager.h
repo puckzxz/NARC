9e319231c5b31112551be4b30ebc5b0be120f539
Chris <cbock1337@gmail.com>
2021-01-27 23:10:32 -0500

    Update WorkspaceManager.h

--- NARC/src/WorkspaceManager.h
+++ NARC/src/WorkspaceManager.h
@@ -13,13 +13,6 @@ struct Request
 {
     std::string name;
     std::string type;
-    // static void to_json(json& j, const Request& r) {
-    //     j = json{{"name", r.name}};
-    // }
-    //
-    // static void from_json(const json& j, Request& r) {
-    //     j.at("name").get_to(r.name);
-    // }
     NLOHMANN_DEFINE_TYPE_INTRUSIVE(Request, name, type);
 };
 
@@ -28,18 +21,6 @@ struct Workspace
     std::string name;
     std::vector<Request> requests;
     NLOHMANN_DEFINE_TYPE_INTRUSIVE(Workspace, name, requests);
-    // static void to_json(json& j, const Workspace& w) {
-    //     j = json{{"name", w.name}};
-    //     for (auto& e : w.requests)
-    //     {
-    //         j["requests"].push_back({{"name", e.name}});
-    //     }
-    // }
-    //
-    // static void from_json(const json& j, Workspace& w) {
-    //     j.at("name").get_to(w.name);
-    //     j.at("requests").get_to<std::vector<Request>>(w.requests);
-    // }
 };
 
 struct Workspaces
